ac8fd05df76a7b0f858087cac21f439462557dc5
ghiottoandrea <andreaghiotto9@gmail.com>
2024-04-18 17:37:24 +0200

    Add class construct

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+class Production{
 
+    public $title;
+    public $language;
+    public $vote;
 
-?>
+    public function __construct(public string $title, public string $language, public string $vote)
+
+}
