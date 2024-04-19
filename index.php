544dcfcc9895c18df86505281d6656bb68fcfb69
ghiottoandrea <andreaghiotto9@gmail.com>
2024-04-19 15:37:16 +0200

    Add print in page

--- index.php
+++ index.php
@@ -2,7 +2,55 @@
 
 require_once __DIR__  . '/models/Production.php';
 
-$firstProduction = new Production('First Product', 'italian', '5');
-$secondProduction = new Production('Second Product', 'french', '2');
+$productions = [
+    new Production('Avangers', 'en', '7'),
+    new Production('Star Wars', 'en', '8')
+];
 
-var_dump($firstProduction, $secondProduction);
+//var_dump($productions);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Film production</h1>
+        <div class="row">
+
+            <?php foreach ($productions as $product) : ?>
+
+            <div class="col-6">
+                <div>
+                    <span>Titolo: </span>
+                    <strong> <?php echo $product->title ?>  </strong>
+                </div>
+
+                <div>
+                    <span>Lingua: </span>
+                    <span> <?= $product->language ?> </span>
+                </div>
+
+                <div>
+                    <span>Voto: </span>
+                    <span> <?= $product->vote ?> su 10 </span>
+                </div>
+
+            </div>
+
+            <br>
+
+            <?php endforeach ?>
+
+        </div>
+    </div>
+</body>
+
+</html>
